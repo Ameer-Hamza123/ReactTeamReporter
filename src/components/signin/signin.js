871186a1eb4af22f4fe28b28e181eeb0e606fda5
Your Name <abdulhakimsabir313@gmail.com>
2021-11-26 22:00:00 +0500

    commit
    commit

---
+++ src/components/signin/signin.js
@@ -0,0 +1,90 @@
+import "../signin/signin.scss";
+import 'antd/dist/antd.css'
+import { Form, Input, Button, Checkbox } from 'antd';
+
+const Signin = () => {
+  const onFinish = (values) => {
+    console.log('Success:', values);
+  };
+
+  const onFinishFailed = (errorInfo) => {
+    console.log('Failed:', errorInfo);
+  };
+
+  return (
+   <>
+   <p>
+     Singin
+   </p>
+   
+    <Form className='form'
+      name="basic"
+      labelCol={{
+        span: 8,
+      }}
+      wrapperCol={{
+        span: 16,
+      }}
+      initialValues={{
+        remember: true,
+      }}
+      onFinish={onFinish}
+      onFinishFailed={onFinishFailed}
+      autoComplete="off"
+    >
+      <Form.Item
+        label="Username"
+        name="username"
+        rules={[
+          {
+            required: true,
+            message: 'Please input your username!',
+          },
+        ]}
+      >
+        <Input />
+      </Form.Item>
+
+      <Form.Item
+        label="Password"
+        name="password"
+        rules={[
+          {
+            required: true,
+            message: 'Please input your password!',
+          },
+        ]}
+      >
+        <Input.Password />
+      </Form.Item>
+
+      <Form.Item
+        name="remember"
+        valuePropName="checked"
+        wrapperCol={{
+          offset: 8,
+          span: 16,
+        }}
+      >
+        <Checkbox>Remember me</Checkbox>
+      </Form.Item>
+
+      <Form.Item
+        wrapperCol={{
+          offset: 8,
+          span: 16,
+        }}
+      >
+        <Button type="primary" htmlType="submit">
+          Submit
+        </Button>
+      </Form.Item>
+      <p id='secondpara'>
+  Dont have an account? <span id ='go'>Signup</span></p>
+  {/* <p id='thirdpara'>Signin</p> */}
+    </Form>
+    </>
+  );
+};
+
+export default Signin;
